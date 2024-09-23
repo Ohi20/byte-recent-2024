8abb40d0bc98b40da41e347087cf3afd10793bba
AshrafOhi <ashrafohi20@gmail.com>
2024-09-24 00:08:53 +0600

    navbar-modified

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      className="block font-semibold p-2 lg:p-2 my-2 text-[#ADB7BE] sm:text-xl md:p-0 hover:text-black hover:bg-white"
     >
       {title}
     </Link>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,6 +5,7 @@ import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 import bytelogo from "../../../public/images/bytelogo.png";
+import Image from "next/image";
 
 const navLinks = [
   {
@@ -29,17 +30,13 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        {/* <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          <img src={bytelogo} height={30} width={130} />
-        </Link> */}
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={bytelogo} width={150} height={30} alt="Brainwave" />
-        </a>
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-70">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2 ">
+        {/* Logo */}
+        <Link className="block xl:mr-8" href={"/"}>
+          <Image src={bytelogo} className="w-40 h-40" alt="Byte" />
+        </Link>
+        {/* Mobile Menu */}
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
@@ -57,10 +54,11 @@ const Navbar = () => {
             </button>
           )}
         </div>
+        {/* Nalinks */}
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
-              <li key={index}>
+              <li className="block" key={index}>
                 <NavLink href={link.path} title={link.title} />
               </li>
             ))}
