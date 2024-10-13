26801e2c985e17e393142dbf126d0602b3e3bd90
AshrafOhi <ashrafohi20@gmail.com>
2024-10-13 17:55:45 +0600

    navbar-modified

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block font-semibold p-2 lg:p-2 my-2 text-[#ADB7BE] sm:text-xl md:p-0 hover:text-black hover:bg-white rounded-lg"
+      className="block font-semibold p-2 lg:p-2 my-2 text-white sm:text-sm hover:text-black hover:bg-white rounded-lg"
     >
       {title}
     </Link>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
-import bytelogo from "../../../public/images/bytelogo.png";
+import bytesvg from "../../../public/images/bytesvg.svg";
 import Image from "next/image";
 
 const navLinks = [
@@ -30,12 +30,14 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-70">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2 ">
+    <nav className="fixed mx-auto  border border-[#33353F] top-0 left-0 right-0 z-10 bg-opacity-70 h-15">
+      <div className="flex container flex-wrap items-center justify-between mx-auto  h-15">
         {/* Logo */}
-        <Link className="block xl:mr-8" href={"/"}>
-          <Image src={bytelogo} className="w-40 h-40" alt="Byte" />
-        </Link>
+        <div className="h-10">
+          <Link className="" href={"/"}>
+            <Image src={bytesvg} className="text-white h-10 w-20" alt="Byte" />
+          </Link>
+        </div>
         {/* Mobile Menu */}
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
@@ -56,7 +58,7 @@ const Navbar = () => {
         </div>
         {/* Nalinks */}
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          <ul className="flex md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li className="block font-serif" key={index}>
                 <NavLink href={link.path} title={link.title} />
